a2b7449161ebcaa776340d5049f993f0353e33a2
vdhxi <83151403+vdhxi@users.noreply.github.com>
2025-03-21 23:52:25 +0700

    Supabase for upload/delete image in cloud

--- project/src/main/java/hsf/project/ProjectApplication.java
+++ project/src/main/java/hsf/project/ProjectApplication.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
